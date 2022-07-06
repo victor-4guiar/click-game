1e17893ccf4aca9fe51b1b441bd94295b4dadf5c
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-07-06 13:49:21 -0300

    Finished?

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ let monika = {
 	message: document.createElement('p'),
 	sprite: document.getElementById('sprite'),
 	add(){
+		monika.sprite.classList.remove('plube2');
 		if(this.espera == false){
 			pla.innerHTML = `${plae++}x`;
+			pla.classList.add('ayaluz');
 			this.espera = true;
 			ba.play();
+			monika.message.setAttribute('id', 'granba');
 			con.appendChild(monika.message);
+			monika.sprite.classList.add('plube');
 			monika.message.innerHTML = '<strong>Baa!!</strong>';
 			monika.sprite.src = 'monika_1.png';
 			setTimeout(reabilitar,1*1000);
@@ -26,4 +30,7 @@ function reabilitar(){
 	monika.espera = false;
 	monika.sprite.src = 'monika_0.png';
 	monika.message.innerHTML = '';
+	monika.sprite.classList.remove('plube');
+	monika.sprite.classList.add('plube2');
+	pla.classList.remove('ayaluz');
 }
